dca09d0a4f757142ef444c9c37c0d2b9d54edde1
newbornfrontender <newbornfrontender@gmail.com>
2020-03-10 12:21:04 +0300

    feat: create calculator with function constructor

--- src/index.js
+++ src/index.js
@@ -3,8 +3,33 @@ function eval() {
     return;
 }
 
+const check = (e) => {
+    let brackets = [];
+    let stack = [];
+    let map = {')': '('};
+
+    e.replace(/[\(\)]/g, (m) => m && brackets.push(m));
+
+    for (let i = 0, {length} = brackets; i < length; i++) {
+        if ([')'].includes(brackets[i])) {
+            if (stack.pop() !== map[brackets[i]]) return false;
+        } else {
+            stack.push(brackets[i])
+        }
+    }
+  
+    return !stack.length;
+};
+
 function expressionCalculator(expr) {
-    // write your solution here
+    if (!check(expr)) throw TypeError('ExpressionError: Brackets must be paired');
+
+    const result = new Function(`return ${expr}`)();
+    const isDivideByZero = result == Infinity || expr.match('/ 0') || expr.match('/0');
+
+    if (isDivideByZero) throw new TypeError('TypeError: Division by zero.');
+
+    return result;
 }
 
 module.exports = {
